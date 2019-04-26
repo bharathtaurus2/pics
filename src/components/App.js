ed38a751665daaedef506b43499c40833aecfe6f
Bharath Gopal <c_gopalb@qti.qualcomm.com>
2019-04-26 18:06:59 +0530

    Added initial Components

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Searchbar from './Searchbar';
+
+
+const App = () => {
+    return <div className="ui container" style={{marginTop: '10px'}}><Searchbar /></div>;
+}
+
+export default App;
